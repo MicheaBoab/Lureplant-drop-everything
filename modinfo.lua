5870d8e0cda583eeceb8a30bea3cc79b5b86d463
Bozheng Liu <lb12016297@gmail.com>
2024-05-23 16:40:10 -0500

    initial push
    initial release

---
+++ modinfo.lua
@@ -0,0 +1,21 @@
+-- This information tells other players more about the mod
+name = "Lureplant drop everything"
+description = "Lureplant will not digest/destroy anything that picked up"
+author = "MicheaBoab"
+version = "1.0"
+
+-- This lets other players know if your mod is out of date, update it to match the current version in the game
+
+api_version = 10
+
+dst_compatible = true
+
+-- Can specify a custom icon for this mod!
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+
+all_clients_require_mod = false
+
+client_only_mod = false
+
+server_filter_tags = {}
