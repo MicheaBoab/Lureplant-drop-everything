c7197ad81af6f46da7b5b93c0f9fb99f1fffaa95
Bozheng Liu <lb12016297@gmail.com>
2024-05-27 21:31:17 -0500

    added container setting
    added icebox container setting

--- modinfo.lua
+++ modinfo.lua
@@ -2,7 +2,7 @@
 name = "Lureplant drop everything"
 description = "Lureplant will not digest/destroy anything that picked up"
 author = "MicheaBoab"
-version = "1.0"
+version = "1.1"
 
 -- This lets other players know if your mod is out of date, update it to match the current version in the game
 
@@ -19,3 +19,30 @@ all_clients_require_mod = false
 client_only_mod = false
 
 server_filter_tags = {}
+
+configuration_options = 
+{	
+	--冰箱
+	{
+		name = "",
+		label = "󰀏 Lureplant 食人花 󰀏",
+		hover = "Lureplant",
+		default = 0,
+		options = {
+			{description = "", data = 0},
+		},
+	},
+		{
+		name = "lureplant_config",
+		label = "lureplant container setting 食人花容器设定",
+		hover = "Lureplant config",
+		options =	{
+			{description = "默认", data = false, hover = "Normal"},
+			--{description = "完全保鲜", data = 0, hover = "Food fresh forever(keep current freshness)"},
+			{description = "同冰箱", data = true, hover = "Act as icebox"},
+		},
+		default = false,
+	},
+}
+
+priority = 1.0
--- modmain.lua
+++ modmain.lua
@@ -1,3 +1,11 @@
 PrefabFiles = {
 	"lureplant",
 }
+
+local lureplant = GetModConfigData("lureplant_config");
+
+AddPrefabPostInit("lureplant", function(inst)
+	if lureplant then
+		inst:AddTag("fridge")
+	end
+end)
